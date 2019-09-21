19ac94abff792f9c857b59a907b76b6a4969929f
aliibrahimxx <37051958+aliibrahimxx@users.noreply.github.com>
2019-09-21 15:04:10 +0200

    Add files via upload

---
+++ using keybad with 7segment/using keybad with 7segment/main.c
@@ -0,0 +1,18 @@
+/*
+ * using keybad with 7segment.c
+ *
+ * Created: 14/09/2019 07:50:07 م
+ * Author : ali ibrahim
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+} 
+
